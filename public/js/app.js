3506ec0be26611b123e58489b742d4bfa6fcb430
Matt Stuhring <whereyouatmatt@gmail.com>
2016-08-01 20:25:07 -0700

    create posts and topics complete

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,5 @@
 (function() {
   'use strict';
 
-  $(".button-collapse").sideNav();
-  $('select').material_select();
-
   angular.module('giftGab', []);
 }());
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,11 +1,43 @@
 (function() {
   'use strict';
 
+  $(".button-collapse").sideNav();
+
   const app = angular.module('giftGab');
 
-  app.controller('TopicCtrl', function() {
+  app.controller('TopicCtrl', function($scope) {
     this.topicToAdd = '';
-    this.topics = [];
+    this.selectedTopicId = '';
+    this.topics = [
+      {
+        id: 1,
+        name: 'Kicks'
+      },
+      {
+        id: 2,
+        name: 'Style'
+      },
+      {
+        id: 3,
+        name: 'Art'
+      },
+      {
+        id: 4,
+        name: 'Technology'
+      },
+      {
+        id: 5,
+        name: 'Dopeness'
+      },
+      {
+        id: 6,
+        name: 'Food and Drink'
+      },
+      {
+        id: 7,
+        name: 'Music'
+      }
+    ];
 
     this.addTopic = (topic) => {
       this.topics.push({ name: this.topicToAdd });
@@ -16,15 +48,47 @@
       const i = this.topics.indexOf(top);
       this.topics.splice(i, 1);
     };
+
+    this.getTopicName = (id) => {
+      for (let i = 0; i < this.topics.length; i++) {
+        if (this.topics[i].id ===  id) {
+          return this.topics[i].name;
+        }
+      }
+
+      return '';
+    };
+
+    $scope.$watch('topic.topics.length', () => {
+      $scope.$$postDigest(() => {
+        $('select').material_select();
+        $('select').on('change', (e) => {
+          this.selectedTopicId = Number.parseInt($(e.target).val());
+        })
+      })
+    });
   });
 
 
   app.controller('PostCtrl', function() {
-    this.postToAdd = '';
+    this.postToAdd = {};
     this.posts = [];
 
-    this.addPost = () => {
-      this.items.push({ name: this.posts });
+    this.addPost = (topicId) => {
+      this.posts.push(
+        {
+          topicId: topicId,
+          title: this.postToAdd.title,
+          image: this.postToAdd.image,
+          gab: this.postToAdd.gab
+        }
+      );
+
+      console.log(this.posts);
+      this.postToAdd.title = '';
+      this.postToAdd.topicId = '';
+      this.postToAdd.image = '';
+      this.postToAdd.gab = '';
     };
   });
 }());
